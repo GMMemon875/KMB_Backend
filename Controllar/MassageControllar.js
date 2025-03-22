40a7f9854816efb46ab5472f28314adbaf3d28bc
Ghulam Mustafa Memon <ghulammemon875@gmail.com>
2025-03-23 01:25:50 +0500

    changes Message

--- Controllar/MassageControllar.js
+++ Controllar/MassageControllar.js
@@ -1,27 +1,22 @@
-import { Message } from "../modals/massageSchema.js"
-import {catchAsyncErrors} from "../middlewere/CatchAsynicError.js"
-import ErrorHandler from "../middlewere/errorMiddlewere.js"
+import { Message } from "../modals/massageSchema.js";
+import { catchAsyncErrors } from "../middlewere/CatchAsynicError.js";
+import ErrorHandler from "../middlewere/errorMiddlewere.js";
 
- export const Sendmassage = catchAsyncErrors(async(req,res,next)=>{
-    const {firstName,lastName,email,phone,message}= req.body
-    if (!firstName || !lastName || !email || !phone || !message) {
-        return next(new ErrorHandler("please fill full Form",400))
-    }
-    await Message.create({firstName,lastName,email,phone,message})
-     res.status(400).json({
-        success: true,
-        massage:"form create successfully"
-    })
- })
-
-
- 
-
-export const getAllMessages = catchAsyncErrors(async(req,res,next)=>{
-    const Messages = await Message.find()
-    res.status(200).json({
-      success:true,
-      Messages,
-    })
-  })
-  
+export const Sendmassage = catchAsyncErrors(async (req, res, next) => {
+  const { firstName, lastName, email, phone, message } = req.body;
+  if (!firstName || !lastName || !email || !phone || !message) {
+    return next(new ErrorHandler("please fill full Form", 400));
+  }
+  await Message.create({ firstName, lastName, email, phone, message });
+  res.status(400).json({
+    success: true,
+    massage: "Send Message successfully",
+  });
+});
+export const getAllMessages = catchAsyncErrors(async (req, res, next) => {
+  const Messages = await Message.find();
+  res.status(200).json({
+    success: true,
+    Messages,
+  });
+});
--- DetaBase/dataBase.js
+++ DetaBase/dataBase.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
-export const database = ()=>{
-    mongoose.connect(process.env.MONGODB_URL,{
-        dbName:"Hospital" 
-
-    }).then(()=>{
-            console.log("database is connected")
-    }) .catch((err)=>{
-        console.log("No dataBase is not connected :",err)
+export const database = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      dbName: "KMB_Database",
     })
-} 
+    .then(() => {
+      console.log("database is connected");
+    })
+    .catch((err) => {
+      console.log("No dataBase is not connected :", err);
+    });
+};
 
 export default database;
